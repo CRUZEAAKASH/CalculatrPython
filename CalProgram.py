c5859dfbeda5971f47ca35906c4ced77696974ed
aakash <aakash.shinghal@gmail.com>
2019-07-15 14:13:25 +0530

    added operator visibility on the textbox

--- CalProgram.py
+++ CalProgram.py
@@ -33,6 +33,13 @@ def undo():
         display.insert(0, "Error")
 
 
+# Display Operator
+def operator(opr):
+    global i
+    i += len(opr)
+    display.insert(i, opr)
+
+
 # Adding textbox
 display = Entry(window)
 display.grid(row=0, columnspan=6, sticky=W + E)
@@ -51,22 +58,22 @@ Button(window, text="8", command=lambda: get_variables(8)).grid(row=3, column=1)
 Button(window, text="9", command=lambda: get_variables(9)).grid(row=3, column=2)
 
 Button(window, text="AC", command=lambda: clear_all()).grid(row=4, column=0)
-Button(window, text="0", command=lambda: get_variables(10)).grid(row=4, column=1)
+Button(window, text="0", command=lambda: get_variables(0)).grid(row=4, column=1)
 Button(window, text="=").grid(row=4, column=2)
 
-Button(window, text="+").grid(row=1, column=3)
-Button(window, text="-").grid(row=2, column=3)
-Button(window, text="*").grid(row=3, column=3)
-Button(window, text="/").grid(row=4, column=3)
+Button(window, text="+", command=lambda: operator("+")).grid(row=1, column=3)
+Button(window, text="-", command=lambda: operator("-")).grid(row=2, column=3)
+Button(window, text="*", command=lambda: operator("*")).grid(row=3, column=3)
+Button(window, text="/", command=lambda: operator("/")).grid(row=4, column=3)
 
-Button(window, text="pi").grid(row=1, column=4)
-Button(window, text="%").grid(row=2, column=4)
-Button(window, text="(").grid(row=3, column=4)
-Button(window, text="exp").grid(row=4, column=4)
+Button(window, text="pi", command=lambda: operator("3.14")).grid(row=1, column=4)
+Button(window, text="%", command=lambda: operator("%")).grid(row=2, column=4)
+Button(window, text="(", command=lambda: operator("(")).grid(row=3, column=4)
+Button(window, text="exp", command=lambda: operator("**")).grid(row=4, column=4)
 
 Button(window, text="<-", command=lambda: undo()).grid(row=1, column=5)
 Button(window, text="x!").grid(row=2, column=5)
-Button(window, text=")").grid(row=3, column=5)
-Button(window, text="^2").grid(row=4, column=5)
+Button(window, text=")", command=lambda: operator(")")).grid(row=3, column=5)
+Button(window, text="^2", command=lambda: operator("**2")).grid(row=4, column=5)
 
 window.mainloop()
